f7f619c4b6eab2122397f2cd92039b94d24b5a65
Naythankik <dolapokiki3ird@gmail.com>
2024-05-29 23:31:59 +0100

    initial commit

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const jobListings = async () => {
     const article = document.createElement("article");
     const langContainer = document.createElement("div");
     langContainer.setAttribute("class", "article-right-side");
+    const role = document.createElement("span");
+    role.setAttribute("class", "tags");
+    role.innerHTML = element.role;
+    langContainer.appendChild(role);
+
+    const level = document.createElement("span");
+    level.setAttribute("class", "tags");
+    level.innerHTML = element.level;
+    langContainer.appendChild(level);
+
+    for (const tools of element.tools) {
+      const span = document.createElement("span");
+      span.setAttribute("class", "tags");
+      span.innerHTML = tools;
+      langContainer.appendChild(span);
+    }
 
     for (const language of element.languages) {
       const span = document.createElement("span");
@@ -30,7 +46,7 @@ const jobListings = async () => {
                   element.featured ? "d-block" : "d-none"
                 }">${element.featured ? "Featured" : ""}</span>
               </div>
-              <h2 class="article-title">Senior Frontend Developer</h2>
+              <h2 class="article-title">${element.position}</h2>
               <ul class="bottom-details">
                 <li>${element.postedAt}</li>
                 <li>${element.contract}</li>
